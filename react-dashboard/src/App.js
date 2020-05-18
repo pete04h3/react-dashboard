ece6f999563e4633bfdb2e6365cf3a041c9b32e5
peterpaN1k <peetmj@gmail.com>
2020-05-18 21:04:25 +0200

    began datavisualization

---
+++ react-dashboard/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Circle from "react-circle";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Circle
+        animate={true} // Boolean: Animated/Static progress
+        responsive={true} // Boolean: Make SVG adapt to parent size
+        size={150} // Number: Defines the size of the circle.
+        lineWidth={14} // Number: Defines the thickness of the circle's stroke.
+        progress={69} // Number: Update to change the progress and percentage.
+        progressColor="cornflowerblue" // String: Color of "progress" portion of circle.
+        bgColor="whitesmoke" // String: Color of "empty" portion of circle.
+        textColor="hotpink" // String: Color of percentage text color.
+        textStyle={{
+          font: "bold 5rem Helvetica, Arial, sans-serif", // CSSProperties: Custom styling for percentage.
+        }}
+        percentSpacing={10} // Number: Adjust spacing of "%" symbol and number.
+        roundedStroke={true} // Boolean: Rounded/Flat line ends
+        showPercentage={true} // Boolean: Show/hide percentage.
+        showPercentageSymbol={true} // Boolean: Show/hide only the "%" symbol.
+      />
+    </div>
+  );
+}
+
+export default App;
